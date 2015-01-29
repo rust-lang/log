3ba35a979c5908d40a12c29a9b64f3d31fac0c9b
Alex Crichton <alex@alexcrichton.com>
2015-01-29 08:39:48 -0800

    Add some feature attributes

--- src/lib.rs
+++ src/lib.rs
@@ -137,11 +137,13 @@
 //!     })
 //! }
 //! ```
+
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico",
        html_root_url = "http://doc.rust-lang.org/log/")]
 #![warn(missing_docs)]
-#![allow(unstable)]
+#![feature(core, std_misc)]
+#![cfg_attr(test, feature(collections))]
 
 use std::ascii::AsciiExt;
 use std::cmp;
