f9a42ca139745843866fc85c4c3a0731a523891f
Alex Crichton <alex@alexcrichton.com>
2015-03-14 11:03:55 -0700

    Update to rust master

--- env/src/lib.rs
+++ env/src/lib.rs
@@ -127,7 +127,6 @@
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico",
        html_root_url = "http://doc.rust-lang.org/env_logger/")]
-#![feature(io)]
 #![cfg_attr(test, deny(warnings))]
 
 extern crate regex;
