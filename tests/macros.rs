6dab6ffb545e4d6a0459652cab28cf5ef41667fa
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-09-02 02:01:22 +0200

    push broken test
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- tests/macros.rs
+++ tests/macros.rs
@@ -7,7 +7,12 @@ fn kv_info() {
     info!("hello",);
     info!("hello {}", "cats");
     info!("hello {}", "cats",);
+    info!("hello {}", "cats",);
     info!("hello {}", "cats", {
+        cat_1: "chashu",
+        cat_2: "nori",
+    });
+    info!("hello {value}", value = "cats", {
         cat_1: "chashu",
         cat_2: "nori",
     });
