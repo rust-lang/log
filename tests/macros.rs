1a7ffda379de76248c8c90678f0c5b6f97d4087c
Ashley Mannix <ashleymannix@live.com.au>
2019-12-16 10:09:37 +1000

    fix up macro use in expr context

--- tests/macros.rs
+++ tests/macros.rs
@@ -8,10 +8,22 @@ fn base() {
 }
 
 #[test]
+fn base_expr_context() {
+    let _ = info!("hello");
+}
+
+#[test]
 fn with_args() {
     info!("hello {}", "cats");
     info!("hello {}", "cats",);
     info!("hello {}", "cats",);
+}
+
+#[test]
+fn with_args_expr_context() {
+    match "cats" {
+        cats => info!("hello {}", cats),
+    };
 }
 
 #[test]
@@ -20,4 +32,14 @@ fn kv() {
         cat_1: "chashu",
         cat_2: "nori",
     });
+}
+
+#[test]
+fn kv_expr_context() {
+    match "chashu" {
+        cat_1 => info!("hello {}", "cats", {
+            cat_1: cat_1,
+            cat_2: "nori",
+        }),
+    };
 }
