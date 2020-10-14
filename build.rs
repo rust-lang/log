14335ec8890f957ade7dc82fcac4f3cbbfec3ece
Matt Brubeck <mbrubeck@limpet.net>
2020-10-14 10:05:02 -0700

    Remove broken const_type_id feature
    The `const_type_id` feature was planned to stabilize in Rust 1.47, but
    this was reverted in: https://github.com/rust-lang/rust/pull/77083
    
    This causes errors when building `log` with the `kv_unstable` feature on
    Rust 1.47 or later.  This patch removes the use of this no-longer-stable
    feature in those Rust versions.

--- build.rs
+++ build.rs
@@ -2,19 +2,13 @@
 //! atomics and sets `cfg` flags accordingly.
 
 use std::env;
-use std::process::Command;
-use std::str::{self, FromStr};
+use std::str;
 
 #[cfg(feature = "kv_unstable")]
 #[path = "src/kv/value/internal/cast/primitive.rs"]
 mod primitive;
 
 fn main() {
-    let minor = match rustc_minor_version() {
-        Some(minor) => minor,
-        None => return,
-    };
-
     let target = match rustc_target() {
         Some(target) => target,
         None => return,
@@ -26,11 +20,6 @@ fn main() {
 
     if target_has_atomics(&target) {
         println!("cargo:rustc-cfg=has_atomics");
-    }
-
-    // If the Rust version is at least 1.46.0 then we can use type ids at compile time
-    if minor >= 47 {
-        println!("cargo:rustc-cfg=const_type_id");
     }
 
     // Generate sorted type id lookup
@@ -60,34 +49,4 @@ fn target_has_atomics(target: &str) -> bool {
 
 fn rustc_target() -> Option<String> {
     env::var("TARGET").ok()
-}
-
-// From the `serde` build script
-fn rustc_minor_version() -> Option<u32> {
-    let rustc = match env::var_os("RUSTC") {
-        Some(rustc) => rustc,
-        None => return None,
-    };
-
-    let output = match Command::new(rustc).arg("--version").output() {
-        Ok(output) => output,
-        Err(_) => return None,
-    };
-
-    let version = match str::from_utf8(&output.stdout) {
-        Ok(version) => version,
-        Err(_) => return None,
-    };
-
-    let mut pieces = version.split('.');
-    if pieces.next() != Some("rustc 1") {
-        return None;
-    }
-
-    let next = match pieces.next() {
-        Some(next) => next,
-        None => return None,
-    };
-
-    u32::from_str(next).ok()
 }
