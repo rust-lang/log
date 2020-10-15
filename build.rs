dec098261819771ae1f22cd85f306ec71f23fd85
Ashley Mannix <kodraus@hey.com>
2020-10-15 17:51:20 +1000

    remove dependency on reverted const type id

--- build.rs
+++ build.rs
@@ -10,11 +10,6 @@ use std::str::{self, FromStr};
 mod primitive;
 
 fn main() {
-    let minor = match rustc_minor_version() {
-        Some(minor) => minor,
-        None => return,
-    };
-
     let target = match rustc_target() {
         Some(target) => target,
         None => return,
@@ -26,11 +21,6 @@ fn main() {
 
     if target_has_atomics(&target) {
         println!("cargo:rustc-cfg=has_atomics");
-    }
-
-    // If the Rust version is at least 1.46.0 then we can use type ids at compile time
-    if minor >= 47 {
-        println!("cargo:rustc-cfg=const_type_id");
     }
 
     // Generate sorted type id lookup
@@ -60,34 +50,4 @@ fn target_has_atomics(target: &str) -> bool {
 
 fn rustc_target() -> Option<String> {
     env::var("TARGET").ok()
-}
-
-// From the `serde` build script
-fn rustc_minor_version() -> Option<u32> {
-    let rustc = match env::var_os("RUSTC") {
-        Some(rustc) => rustc,
-        None => return None,
-    };
-
-    let output = match Command::new(rustc).arg("--version").output() {
-        Ok(output) => output,
-        Err(_) => return None,
-    };
-
-    let version = match str::from_utf8(&output.stdout) {
-        Ok(version) => version,
-        Err(_) => return None,
-    };
-
-    let mut pieces = version.split('.');
-    if pieces.next() != Some("rustc 1") {
-        return None;
-    }
-
-    let next = match pieces.next() {
-        Some(next) => next,
-        None => return None,
-    };
-
-    u32::from_str(next).ok()
 }
