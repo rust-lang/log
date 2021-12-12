77667f5436eaf507dadad0c3ba523c45354c72e4
KodrAus <kodraus@hey.com>
2021-12-12 10:57:45 +1000

    move implicit arg tests into module

--- tests/macros.rs
+++ tests/macros.rs
@@ -91,35 +91,6 @@ fn named_args() {
 }
 
 #[test]
-#[rustversion::since(1.58)]
-fn implicit_named_args() {
-    let world = "world";
-
-    for lvl in log::Level::iter() {
-        log!(lvl, "hello {world}");
-        log!(lvl, "hello {world}",);
-
-        log!(target: "my_target", lvl, "hello {world}");
-        log!(target: "my_target", lvl, "hello {world}",);
-
-        log!(lvl; "hello {world}");
-        log!(lvl; "hello {world}",);
-
-        log!(target = "my_target", lvl; "hello {world}");
-        log!(target = "my_target", lvl; "hello {world}",);
-    }
-
-    all_log_macros!("hello {world}");
-    all_log_macros!("hello {world}",);
-
-    all_log_macros!(target = "my_target"; "hello {world}");
-    all_log_macros!(target = "my_target"; "hello {world}",);
-
-    all_log_macros!(target: "my_target", "hello {world}");
-    all_log_macros!(target: "my_target", "hello {world}",);
-}
-
-#[test]
 fn enabled() {
     for lvl in log::Level::iter() {
         let _enabled = if log_enabled!(target: "my_target", lvl) {
@@ -184,26 +155,65 @@ fn kv_named_args() {
 
 #[test]
 #[cfg(feature = "kv_unstable")]
-#[rustversion::since(1.58)]
-fn kv_implicit_named_args() {
-    let world = "world";
-
-    for lvl in log::Level::iter() {
-        log!(target = "my_target", lvl; cat_1 = "chashu", cat_2 = "nori"; "hello {world}");
-
-        log!(lvl; cat_1 = "chashu", cat_2 = "nori"; "hello {world}");
-    }
-
-    all_log_macros!(target = "my_target"; cat_1 = "chashu", cat_2 = "nori"; "hello {world}");
-    all_log_macros!(cat_1 = "chashu", cat_2 = "nori"; "hello {world}");
-}
-
-#[test]
-#[cfg(feature = "kv_unstable")]
 fn kv_expr_context() {
     match "chashu" {
         cat_1 => {
             info!(target = "target"; cat_1 = cat_1, cat_2 = "nori"; "hello {}", "cats")
         }
     };
+}
+
+#[test]
+#[cfg(feature = "kv_unstable")]
+fn kv_shadow_target() {
+    all_log_macros!(target = "kv_target"; "hello {}", "world");
+    all_log_macros!(target = "kv_target", cat_1 = "chashu", cat_2 = "nori"; "hello {}", "world");
+}
+
+#[rustversion::since(1.58)]
+mod implicit_args {
+    use super::*;
+
+    #[test]
+    fn implicit_named_args() {
+        let world = "world";
+    
+        for lvl in log::Level::iter() {
+            log!(lvl, "hello {world}");
+            log!(lvl, "hello {world}",);
+    
+            log!(target: "my_target", lvl, "hello {world}");
+            log!(target: "my_target", lvl, "hello {world}",);
+    
+            log!(lvl; "hello {world}");
+            log!(lvl; "hello {world}",);
+    
+            log!(target = "my_target", lvl; "hello {world}");
+            log!(target = "my_target", lvl; "hello {world}",);
+        }
+    
+        all_log_macros!("hello {world}");
+        all_log_macros!("hello {world}",);
+    
+        all_log_macros!(target = "my_target"; "hello {world}");
+        all_log_macros!(target = "my_target"; "hello {world}",);
+    
+        all_log_macros!(target: "my_target", "hello {world}");
+        all_log_macros!(target: "my_target", "hello {world}",);
+    }
+
+    #[test]
+    #[cfg(feature = "kv_unstable")]
+    fn kv_implicit_named_args() {
+        let world = "world";
+
+        for lvl in log::Level::iter() {
+            log!(target = "my_target", lvl; cat_1 = "chashu", cat_2 = "nori"; "hello {world}");
+
+            log!(lvl; cat_1 = "chashu", cat_2 = "nori"; "hello {world}");
+        }
+
+        all_log_macros!(target = "my_target"; cat_1 = "chashu", cat_2 = "nori"; "hello {world}");
+        all_log_macros!(cat_1 = "chashu", cat_2 = "nori"; "hello {world}");
+    }
 }
