d1a8306aadb88d56b74c73cdce4ef0153fb549cb
KodrAus <kodraus@hey.com>
2024-06-27 07:33:32 +1000

    prepare for 0.4.22 release

--- src/lib.rs
+++ src/lib.rs
@@ -336,7 +336,7 @@
 #![doc(
     html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
     html_favicon_url = "https://www.rust-lang.org/favicon.ico",
-    html_root_url = "https://docs.rs/log/0.4.21"
+    html_root_url = "https://docs.rs/log/0.4.22"
 )]
 #![warn(missing_docs)]
 #![deny(missing_debug_implementations, unconditional_recursion)]
